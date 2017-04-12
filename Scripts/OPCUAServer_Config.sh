a58d67de561a1ebe1c4969985f175d34be8178ee
TimtationOne <timebner@gmx.de>
2017-04-12 16:31:11 +0200

    modiefied script

--- Scripts/OPCUAServer_Config.sh
+++ Scripts/OPCUAServer_Config.sh
@@ -4,5 +4,5 @@ sudo apt-get -y install subversion git
 cd ~
 mkdir $TOSCAHOME
 cd $TOSCAHOME
-svn export https://github.com/TimtationOne/TOSCA4IoT_Engine.git/trunk/OPC_UA
+svn export https://github.com/TimtationOne/TOSCA4IoT_Engine.git/trunk/Appliations/OPC_UA
 cd ~

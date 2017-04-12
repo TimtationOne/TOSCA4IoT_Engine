c6ae55b2af121faacabf04078e9ba8afe40dc05f
TimtationOne <timebner@gmx.de>
2017-04-12 17:11:22 +0200

    modiefied script

--- Scripts/OPCUAServer_Start.sh
+++ Scripts/OPCUAServer_Start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TOSCAHOME="/home/pi/tosca4iot"
-nohup uaserver -x $TOSCAHOME/OPC_UA/Weather_Sensor_v0.1 > $TOSCAHOME/opc_ua.log & #>>/dev/null 2>>/dev/null &
+uaserver -x $TOSCAHOME/OPC_UA/Weather_Sensor_v0.1 > $TOSCAHOME/opc_ua.log $
--- Scripts/TISensorTag2OPCUA_Install.sh
+++ Scripts/TISensorTag2OPCUA_Install.sh
@@ -7,18 +7,16 @@ wget https://nodejs.org/dist/v5.10.1/node-v5.10.1-linux-armv7l.tar.gz
 tar -xvf node-v5.10.1-linux-armv7l.tar.gz -C /usr/local/lib
 export PATH=${PATH}:/usr/local/lib/node-v5.10.1-linux-armv7l/bin
 
+#Git Installation
+TOSCAHOME="/home/pi/tosca4iot"
+sudo apt-get -y install subversion git
+cd ~
+mkdir $TOSCAHOME
+cd $TOSCAHOME
+svn export https://github.com/TimtationOne/TOSCA4IoT_Engine.git/trunk/Applications/TISensorTag2OPCUA
+cd TISensorTag2OPCUA
 
 #Install Libraries
 npm install sensortag
 
-
-#Git Installation
-HOME="/home/pi"
-apt-get -y install subversion git
 cd ~
-mkdir $HOME/UseCase
-cd $HOME/UseCase/
-svn export https://github.com/steilerDev/TOSCA4IoT.git/trunk/OPC_UA
-pip install freeopcua
-nohup uaserver -x /home/pi/UseCase/OPC_UA/Weather_Sensor_v0.1 >>/dev/null 2>>/dev/null &
-nohup node /home/pi/UseCase/OPC_UA/SensorTag_Connect/sensortag2opcua.js >>/dev/null 2>>/dev/null &

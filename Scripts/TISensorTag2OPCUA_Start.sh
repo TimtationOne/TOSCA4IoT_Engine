a5d31c1f3a887af1fe7909b205af3c1d435961f1
TimtationOne <timebner@gmx.de>
2017-04-15 15:26:08 +0200

    Updated Scripts

--- Scripts/TISensorTag2OPCUA_Start.sh
+++ Scripts/TISensorTag2OPCUA_Start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-nohup /usr/local/lib/node-v5.10.1-linux-armv7l/bin/node /home/pi/tosca4iot/TISensorTag2OPCUA/sensortag2opcua.js $1 > /home/pi/tosca4iot/sensortag.log & #>>/dev/null 2>>/dev/null &
+TOSCAHOME="/home/pi/tosca4iot"
+chmod 777 $TOSCAHOME/weathersensor.log
+nohup /usr/local/lib/node-v5.10.1-linux-armv7l/bin/node /home/pi/tosca4iot/TISensorTag2OPCUA/sensortag2opcua.js > $TOSCAHOME/weathersensor.log & #>>/dev/null 2>>/dev/null &

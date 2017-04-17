a50d3ee483f6cf4adc4fd9ea2bc2ed8a958bb9f4
TimtationOne <timebner@gmx.de>
2017-04-17 17:03:20 +0200

    Added wait for reboot to engine and added pixels scripts as new application

---
+++ Scripts/Pixels_Install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Install User Interface Pixels
+apt-get -y update
+apt-get -y upgrade
+apt-get install -y --no-install-recommends xserver-xorg
+apt-get install -y --no-install-recommends xinit
+apt-get install -y raspberrypi-ui-mods

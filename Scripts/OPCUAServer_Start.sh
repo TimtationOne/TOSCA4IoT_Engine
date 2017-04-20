7866509d2d482b96f86a0e7e57f966bc782aee42
TimtationOne <timebner@gmx.de>
2017-04-20 11:08:26 +0200

    added nohup

--- Scripts/OPCUAServer_Start.sh
+++ Scripts/OPCUAServer_Start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TOSCAHOME="/home/pi/tosca4iot"
 chmod 777 $TOSCAHOME/opc_ua.log
-uaserver -x $TOSCAHOME/OPC_UA/Weather_Sensor_v0.1.xml > $TOSCAHOME/opc_ua.log &
+nohup uaserver -x $TOSCAHOME/OPC_UA/Weather_Sensor_v0.1.xml > $TOSCAHOME/opc_ua.log &

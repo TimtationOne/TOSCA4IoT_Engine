000439ae40b94b57af1f493296f16e892b702da2
TimtationOne <timebner@gmx.de>
2017-04-20 09:42:52 +0200

    NodeJs Script Change

--- Scripts/TISensorTag2OPCUA_Start.sh
+++ Scripts/TISensorTag2OPCUA_Start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 TOSCAHOME="/home/pi/tosca4iot"
 chmod 777 $TOSCAHOME/weathersensor.log
-nohup /usr/local/lib/node-v5.10.1-linux-armv7l/bin/node /home/pi/tosca4iot/TISensorTag2OPCUA/sensortag2opcua.js > $TOSCAHOME/weathersensor.log & #>>/dev/null 2>>/dev/null &
+#nohup /usr/local/lib/node-v5.10.1-linux-armv7l/bin/node /home/pi/tosca4iot/TISensorTag2OPCUA/sensortag2opcua.js > $TOSCAHOME/weathersensor.log & #>>/dev/null 2>>/dev/null &
+nohup node /home/pi/tosca4iot/TISensorTag2OPCUA/sensortag2opcua.js > $TOSCAHOME/weathersensor.log & #>>/dev/null 2>>/dev/null &

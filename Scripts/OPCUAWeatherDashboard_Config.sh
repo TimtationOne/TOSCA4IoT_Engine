fa041e88942988ae1f6e5fac99077bcf272702cc
TimtationOne <timebner@gmx.de>
2017-04-21 13:55:11 +0200

    Dashboard Commit

--- Scripts/OPCUAWeatherDashboard_Config.sh
+++ Scripts/OPCUAWeatherDashboard_Config.sh
@@ -5,5 +5,6 @@ cat > $TOSCAHOME/OPCUAWeatherDashboard/config.properties <<EOL
 OPC_UA_URL=$1
 EOL
 
+mkdir /home/pi/.config/
 mkdir /home/pi/.config/autostart
 mv $TOSCAHOME/OPCUAWeatherDashboard/dashboard.desktop /home/pi/.config/autostart/
